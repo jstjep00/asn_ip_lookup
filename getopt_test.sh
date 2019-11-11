40cc6bae2b39b42db24df7d839cb824df18b8eb7
Josip Stjepanović <57007759+jstjep00@users.noreply.github.com>
2019-11-11 10:29:17 +0100

    Update getopt_test.sh

--- getopt_test.sh
+++ getopt_test.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
+
+usage(){ 
+    echo "Usage: $0 [-iL input_list.txt] [-oL output_list.txt] [-h]" 1>&2;
+    echo -e "\n" 1>&2;
+    echo -e "-iL, --input-list: input list is a text file that has ASs for each new line in file" 1>&2;
+    echo -e "-oL, --output-list: if output list isn't specified then script \n\t will write and replace an output.txt file in current working directory" 1>&2;
+    echo -e "-h, --help: help command that shows you how to use the script
+    exit 1; 
+}
+
+while getopts ":iL:oL:" o; do
+    case "${o}" in
+        iL)
+            iL=${OPTARG}
+            if 
+            ;;
+        oL)
+            oL=${OPTARG}
+            ;;
+        h)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${iL}" ] && [ -z "${p}" ]; then
+    usage
+fi
 
